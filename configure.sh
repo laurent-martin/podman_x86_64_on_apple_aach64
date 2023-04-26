5ef47a55e5b1193a61cbd5e7d4863118bf5b855e
Laurent Martin <laurent.martin.l@gmail.com>
2023-04-26 15:35:03 +0200

    merge configure and start

--- configure.sh
+++ configure.sh
@@ -28,11 +28,26 @@ mylog(){
   *) c=9;;
   esac
   shift
-  echo $w "$(tput setaf $c)$p$@$s$(tput op)"
+  echo -e $w "$(tput setaf $c)$p$@$s$(tput op)"
 }
 
-# main
+# extract parameter from config
+get_parameter(){
+  if test -z "$config";then
+    config="$(podman machine inspect $NAME)"
+  fi
+  echo "$config"|jq -r ".[0].$1"
+}
+
+# Create machine
 create_machine(){
+  mylog check "Checking if machine does not already exist"
+  if podman machine inspect $NAME 2> /dev/null 1>&2;then
+    mylog warn "Machine already exists: $NAME" 1>&2
+    users_home="$HOME"
+    config_file=$(podman machine inspect $NAME|jq -r '.[0].ConfigPath.Path')
+    return
+  fi
   # latest stable coreos
   latest_coreos_url=$(curl -s https://builds.coreos.fedoraproject.org/streams/stable.json|jq -r '.architectures.x86_64.artifacts.qemu.formats."qcow2.xz".disk.location')
 
@@ -70,36 +85,94 @@ create_machine(){
     -e '/"-accel",/ d'     `#-accel: remove, use default (tcg)`\
     -e '/"-M",/ d'         `#-machine: remove, use default (pc)`\
     "$config_file"
+
+  mount_tag=$(sed -nEe "s|.*path=$users_home,mount_tag=([^,]+),.*|\1|p" "$config_file")
+  if test -z "$mount_tag";then
+    mylog error "No mount tag" 1>&2
+    exit 1
+  fi
+  echo "If the user volume mount does not work, you can manually mount with:"
+  echo "podman machine ssh $NAME sudo mount -t 9p -o trans=virtio,version=9p2000.L $mount_tag $users_home"
+  echo
+  echo "Start the machine with:"
+  echo "$0 start $NAME"
 }
-mylog check "Checking jq"
-if jq --version > /dev/null 2>&1;then mylog ok ", jq found";else
-  mylog error "Please install jq" 1>&2
-  exit 1
-fi
 
-mylog check "Checking podman"
-if podman -v > /dev/null 2>&1;then mylog ok ", podman found";else
-  mylog error "Please install podman" 1>&2
-  exit 1
-fi
+start_machine(){
+  # Check if machine exists
+  podman machine inspect $NAME > /dev/null || exit 1
+  # Check state, propose to reset if state is "starting"
+  conf_file=$(get_parameter ConfigPath.Path)
+  state=$(get_parameter State)
+  case "$state" in
+  *ing)
+      if test "$2" = -f;then
+          mylog warn "Resetting state." 1>&2
+          tmp=$(mktemp)
+          jq '.Starting = false' $conf_file > "$tmp" && mv "$tmp" $conf_file
+      else
+          mylog error "Machine state: $state" 1>&2
+          mylog warn "To reset state and force start, execute:" 1>&2
+          mylog warn "$0 $* -f" 1>&2
+          exit 1
+      fi
+      ;;
+  esac
+  # get machine's SSH port
+  export ssh_port=$(get_parameter SSHConfig.Port)
+  # start machine in background
+  podman machine start $NAME &
+  # get podman's PID
+  export podmanpid=$!
+  # give some time to podman to start qemu
+  sleep 3
+  # stop podman (pause)
+  mylog info 'Pausing podman'
+  kill -STOP $podmanpid
+  # wait until the SSH server on the qemu vm is available
+  while true ;do for spinner in - \\ \| /; do
+      # if podman exited due to error, stop waiting
+      if ! ps -p $podmanpid > /dev/null;then
+          echo 'ERROR: podman exited prematurely'
+          exit 1
+      fi
+      # check if SSH is listening
+      if (sleep 1;echo)|nc localhost $ssh_port|grep --quiet '^SSH';then
+          # resume podman process
+          mylog info "\rSSH available: Resuming podman."
+          kill -CONT $podmanpid
+          break 2
+      fi
+      #echo -en "\rWaiting for SSH $spinner"
+      mylog check "\rWaiting for SSH $spinner"
+      sleep 2
+  done;done&
+  # wait for subprocesses to complete
+  wait
+}
 
-mylog check "Checking if machine does not already exist"
-if ! podman machine inspect $NAME 2> /dev/null 1>&2;then
-  mylog ok ", machine does not exist"
-  create_machine
-else
-  mylog warn "Machine already exists: $NAME" 1>&2
-  users_home="$HOME"
-  config_file=$(podman machine inspect $NAME|jq -r '.[0].ConfigPath.Path')
-fi
+check_tool(){
+  local tool=$1
+  local option=$2
+  mylog check "Checking $tool"
+  if $tool $option > /dev/null 2>&1;then mylog ok ", $tool found";else
+    mylog error "Please install $tool" 1>&2
+    exit 1
+  fi
+}
 
-mount_tag=$(sed -nEe "s|.*path=$users_home,mount_tag=([^,]+),.*|\1|p" "$config_file")
-if test -z "$mount_tag";then
-  mylog error "No mount tag" 1>&2
-  exit 1
-fi
-echo "If the user volume mount does not work, you can manually mount with:"
-echo "podman machine ssh $NAME sudo mount -t 9p -o trans=virtio,version=9p2000.L $mount_tag $users_home"
-echo
-echo "Start the machine with:"
-echo "./start.sh $NAME"
+command="$1"
+shift
+
+case "$command" in
+  create|start)
+    check_tool jq --version
+    check_tool podman -v
+    check_tool curl -V
+    ${command}_machine "$@"
+    ;;
+  *)
+    echo "Usage: $0 <create|start> [-f]" 1>&2
+    exit 1
+    ;;
+esac
